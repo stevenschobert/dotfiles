d35b22ca1d52aa651ee755c3afd466e8b400b900
Steven Schobert <spschobert@gmail.com>
2015-03-12 13:28:40 -0500

    remove old aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,13 +3,6 @@ alias o="open ."
 alias showHiddenFiles="defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder"
 alias hideHiddenFiles="defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder"
 
-# bundler
-alias b="bundle"
-
-# postgres
-alias pg_start="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
-alias pg_stop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
-
 # docker
 function dk_clean {docker rm `docker ps --no-trunc -a -q` }
 function export_boot2docker { echo $(boot2docker shellinit) > ~/.zsh/boot2docker.zsh }
