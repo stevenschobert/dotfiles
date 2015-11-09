7646e50df3d1bc115f22300b1f447e7daf1f98cc
Steven Schobert <spschobert@gmail.com>
2015-11-09 01:27:10 -0600

    [node] add shim step to global install

--- .node/install-global.sh
+++ .node/install-global.sh
@@ -1,3 +1,5 @@
+BASEDIR=$(dirname $0)
+
 # keeps a list of npm packages I always want installed globally
 npm install -g \
     bower \
@@ -9,3 +11,7 @@ npm install -g \
     lodash \
     metalsmith \
     tern
+
+# updates shimmed asdf path with global modules
+echo "export NODE_PATH=\$NODE_PATH:$(npm config get prefix)/lib/node_modules"\
+  > $BASEDIR/path_shim.sh
