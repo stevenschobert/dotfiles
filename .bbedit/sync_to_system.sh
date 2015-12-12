d7417ce5c99ed7522f52015cf83880ae8ba63abd
Steven Schobert <spschobert@gmail.com>
2015-12-11 23:51:56 -0600

    [bbedit] add packages and language modules

--- .bbedit/sync_to_system.sh
+++ .bbedit/sync_to_system.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 THIS_DIR=$(dirname "$0")
-DIRS_TO_COPY=("Text Filters" "Color Schemes")
+DIRS_TO_COPY=("Text Filters" "Color Schemes" "Packages" "Language Modules")
 BBEDIT_PATH="$HOME/Library/Application Support/BBEdit"
 
 for dir in "${DIRS_TO_COPY[@]}"
