237f3cdf2447afd4a04848d1a29f227ee176e8c9
Steven Schobert <spschobert@gmail.com>
2015-12-17 14:57:53 -0600

    update bbedit sync

--- .bbedit/sync_to_system.sh
+++ .bbedit/sync_to_system.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 THIS_DIR=$(dirname "$0")
-DIRS_TO_COPY=("Text Filters" "Color Schemes" "Packages" "Language Modules")
+DIRS_TO_COPY=("Text Filters" "Color Schemes" "Language Modules")
 BBEDIT_PATH="$HOME/Library/Application Support/BBEdit"
 
 for dir in "${DIRS_TO_COPY[@]}"
