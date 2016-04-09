70bd3e679e4527f39d668b3448c8c51f8b660a28
Steven Schobert <spschobert@gmail.com>
2016-04-09 10:32:32 -0500

    [bbedit] add gitx script

--- .bbedit/sync_to_system.sh
+++ .bbedit/sync_to_system.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 THIS_DIR=$(dirname "$0")
-DIRS_TO_COPY=("Text Filters" "Color Schemes" "Language Modules" "Setup")
+DIRS_TO_COPY=("Text Filters" "Color Schemes" "Language Modules" "Setup" "Scripts")
 BBEDIT_PATH="$HOME/Library/Application Support/BBEdit"
 
 for dir in "${DIRS_TO_COPY[@]}"
