0b7feb5127b1ffe30585e710463da0b392fe8a9b
Steven Schobert <spschobert@gmail.com>
2017-03-12 02:58:54 +0000

    [zsh] add find-agent helper for linux

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -1,21 +1,33 @@
+PLATFORMSTR=$(uname -s)
+
 # Path
 export PATH=/usr/local/bin:$PATH
 export PATH=$PATH:vendor/bundle/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 
-# Don't match patterns
-unsetopt nomatch
-
-# asdf
-. $HOME/.asdf/asdf.sh
-
-# node
-. $HOME/.node/path_shim.sh
+# Heroku Toolbelt
+export PATH=$PATH:/usr/local/heroku/bin
 
 # Editor
 export EDITOR=vim
 
-# Heroku Toolbelt
-export PATH=$PATH:/usr/local/heroku/bin
+# Don't match patterns
+unsetopt nomatch
 
-# Android
-export PATH=$PATH:~/Library/Android/sdk/platform-tools:~/Library/Android/sdk/tools
+if [[ "$PLATFORMSTR" == "Darwin" ]]; then
+  # asdf
+  . $HOME/.asdf/asdf.sh
+
+  # node
+  . $HOME/.node/path_shim.sh
+
+  # Android
+  export PATH=$PATH:~/Library/Android/sdk/platform-tools:~/Library/Android/sdk/tools
+fi
+
+if [[ "$PLATFORMSTR" == "Linux" ]]; then
+  # Source ssh-agent finder
+  . ~/.ssh/find-agent.sh
+
+  # Automatically pick the first ssh-agent
+  ssh-find-agent -a || eval $(ssh-agent) > /dev/null
+fi
