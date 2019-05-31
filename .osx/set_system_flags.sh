0dd6f50de0c630a6f3e81361188ae1361d2f4341
Steven Schobert <spschobert@gmail.com>
2019-05-31 09:50:57 -0500

    disable some system flags

--- .osx/set_system_flags.sh
+++ .osx/set_system_flags.sh
@@ -1,6 +1,6 @@
 # Show full path in OS X Finder
 # from https://github.com/herrbischoff/awesome-osx-command-line#finder
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
 # Disable screenshot window shadow
-defaults write com.apple.screencapture disable-shadow -bool true
+# defaults write com.apple.screencapture disable-shadow -bool true
