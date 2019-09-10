784b18f7c97cad86abdaa220c7c62e339be763c4
Steven Schobert <spschobert@gmail.com>
2019-09-10 14:52:19 -0500

    add sublime text settings

---
+++ .sublimetext/backup_to_git.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+THIS_DIR=$(dirname "$0")
+DIRS_TO_COPY=(
+  "Packages"
+)
+SUBLIMETEXT_PATH="$HOME/Library/Application Support/Sublime Text 3"
+
+for dir in "${DIRS_TO_COPY[@]}"
+do
+  rm -rf "$THIS_DIR/$dir"
+  cp -r "$SUBLIMETEXT_PATH/$dir" "$THIS_DIR/$dir"
+done
