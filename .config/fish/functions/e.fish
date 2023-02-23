5b9943249a6565370c14d6b72c22159d04f8fcd6
Steven Schobert <spschobert@gmail.com>
2023-02-22 21:57:52 -0600

    [fish] port edit command from bash

--- .config/fish/functions/e.fish
+++ .config/fish/functions/e.fish
@@ -1,3 +1,15 @@
-function e --wraps=nvim --description 'alias e=nvim'
-  nvim $argv; 
+function e --wraps=bbedit --description 'Open project, file, or directory in BBEdit'
+  set -f rel_path "."
+  if test -n "$argv[1]"
+    set -f rel_path "$argv[1]"
+  end
+  set -f abs_path $(realpath "$rel_path")
+  set -f proj_dir "$HOME/Documents/BBEdit Projects"
+  set -f proj_file_match $(grep --include=project.bbprojectdata -rl "$proj_dir" -e "<string>file://$abs_path/</string>")
+  set -f bb_open_arg "$rel_path"
+  if test -n "$proj_file_match"
+    set -f bb_open_arg $(dirname "$proj_file_match")
+  end
+
+  bbedit "$bb_open_arg"
 end
