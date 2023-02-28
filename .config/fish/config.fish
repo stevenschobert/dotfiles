d0a76132c464dac6a9461fe1d0bd48dcf62d508d
Steven Schobert <spschobert@gmail.com>
2023-02-28 14:04:24 -0600

    [fish] tweak theme colors to match term theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,20 +3,21 @@ if status is-interactive
 end
 
 # TokyoNight Color Palette
-set -l foreground c0caf5
-set -l selection 33467c
+set -l foreground ADADAD
+set -l foreground_bright BABCC8
+set -l selection 2E3758
 set -l comment 565f89
-set -l red f7768e
-set -l orange ff9e64
-set -l yellow e0af68
-set -l green 9ece6a
-set -l purple 9d7cd8
-set -l cyan 7dcfff
-set -l pink bb9af7
+set -l red E36744
+set -l orange E17F64
+set -l yellow EADA4A
+set -l green 7EC452
+set -l purple F258B9
+set -l cyan 4ABFDF
+set -l pink FA83C0
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
+set -g fish_color_command $foreground_bright
 set -g fish_color_keyword $pink
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
@@ -36,42 +37,6 @@ set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
 set -g fish_pager_color_selected_background --background=$selection
-
-## Kanagawa Fish shell theme
-## A template was taken and modified from Tokyonight:
-## https://github.com/folke/tokyonight.nvim/blob/main/extras/fish_tokyonight_night.fish
-#set -l foreground DCD7BA
-#set -l selection 2D4F67
-#set -l comment 727169
-#set -l red C34043
-#set -l orange FF9E64
-#set -l yellow C0A36E
-#set -l green 76946A
-#set -l purple 957FB8
-#set -l cyan 7AA89F
-#set -l pink D27E99
-#
-## Syntax Highlighting Colors
-#set -g fish_color_normal $foreground
-#set -g fish_color_command $cyan
-#set -g fish_color_keyword $pink
-#set -g fish_color_quote $yellow
-#set -g fish_color_redirection $foreground
-#set -g fish_color_end $orange
-#set -g fish_color_error $red
-#set -g fish_color_param $purple
-#set -g fish_color_comment $comment
-#set -g fish_color_selection --background=$selection
-#set -g fish_color_search_match --background=$selection
-#set -g fish_color_operator $green
-#set -g fish_color_escape $pink
-#set -g fish_color_autosuggestion $comment
-#
-## Completion Pager Colors
-#set -g fish_pager_color_progress $comment
-#set -g fish_pager_color_prefix $cyan
-#set -g fish_pager_color_completion $foreground
-#set -g fish_pager_color_description $comment
 
 # ASDF language version manager
 source ~/.asdf/asdf.fish
