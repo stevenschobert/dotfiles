a83253010228d83ee19d94e559e9ea6281d84d35
Steven Schobert <spschobert@gmail.com>
2023-07-21 13:51:19 -0500

    [fish] add default arg to e open command

--- .config/fish/functions/e.fish
+++ .config/fish/functions/e.fish
@@ -1,4 +1,7 @@
 function e --wraps=code --description 'alias e code'
-  code $argv
-        
+  set -f open_args "."
+  if test (count $argv) -gt 0
+      set -f open_args $argv
+  end
+  code $open_args
 end
