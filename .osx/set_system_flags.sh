841b3d428d13ebefd056f1e30ca4b13073358f1f
Steven Schobert <spschobert@gmail.com>
2023-11-15 18:54:27 -0600

    add additional macOS flag

--- .osx/set_system_flags.sh
+++ .osx/set_system_flags.sh
@@ -5,5 +5,8 @@
 # Disable screenshot window shadow
 # defaults write com.apple.screencapture disable-shadow -bool true
 
+# Show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
+
 # Enable fast key-repeat in Sublime Text
 defaults write com.sublimetext.3 ApplePressAndHoldEnabled -bool false
