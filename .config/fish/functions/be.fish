461e789a74a765c664be2f55a2a306268382ca5a
Steven Schobert <spschobert@gmail.com>
2024-02-16 12:19:08 -0600

    [fish] add more bundle exec alias helpers

---
+++ .config/fish/functions/be.fish
@@ -0,0 +1,4 @@
+function be --wraps='bundle exec' --description 'alias be bundle exec'
+  bundle exec $argv
+        
+end
---
+++ .config/fish/functions/ber.fish
@@ -0,0 +1,4 @@
+function ber --wraps='bundle exec rails' --description 'alias ber bundle exec rails'
+  bundle exec rails $argv
+        
+end
